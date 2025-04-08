6ea0073751451ecf7ab8cf9fa2e760c23c16c2e2
imanuelcio <ciociocio009@gmail.com>
2025-04-08 18:24:25 +0700

    remove authOption provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,64 +1,6 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { getUserByEmail, saveUserToDB } from "@/lib/db";
-import bcrypt from "bcryptjs";
 
-export const authOptions: any = {
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const user = (await getUserByEmail(credentials!.email)) as any;
-        if (!user || !user.password) throw new Error("Email/password salah");
-
-        const isValid = await bcrypt.compare(
-          credentials!.password,
-          user.password
-        );
-        if (!isValid) throw new Error("Password salah");
-
-        return { id: user._id.toString(), name: user.name, email: user.email };
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user, account }: any) {
-      const existing = (await getUserByEmail(user.email!)) as any;
-      if (!existing && account?.provider === "google") {
-        await saveUserToDB({
-          name: user.name,
-          email: user.email,
-          password: null,
-          provider: "google",
-          image: user.image,
-        });
-      }
-      return true;
-    },
-    async jwt({ token, user }: any) {
-      if (user) token.id = user.id;
-      return token;
-    },
-    async session({ session, token }: any) {
-      session.user.id = token.id;
-      return session;
-    },
-  },
-  session: {
-    strategy: "jwt",
-    maxAge: 60 * 60,
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "./auth-option";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
